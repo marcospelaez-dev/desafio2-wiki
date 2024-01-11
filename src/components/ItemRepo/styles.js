80a8f72966e7ba0b55a83e4cb51b0a4475f33e3d
Marcos Sousa <pelaez.marcos@gmail.com>
2024-01-11 15:55:48 -0300

    Listagem-repos

---
+++ src/components/ItemRepo/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    width: 95%;
+
+    h3 {
+        font-size: 32px;
+        color: #FAFAFA;
+    }
+
+    p {
+        font-size: 16px;
+        color: #FAFAFA60;
+        margin-bottom: 20px;
+    }
+
+    a.remover {
+        color: #ff000f;
+        margin-top: 20px;
+    } 
+
+    hr {
+        color: #FAFAFA60;
+        margin: 20px 0;
+    }
+
+`
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,7 @@
 
 import gitlogo from '../assets/github.png';
+import Input from '../components/Input';
+import ItemRepo from '../components/ItemRepo';
 
 import { Container } from './styles';
 
@@ -7,6 +9,8 @@ function App() {
   return (
     <Container>
       <img src={gitlogo} width={72} height={72} alt='Logotipo Github' /> 
+      <Input />
+      <ItemRepo />
     </Container>
   );
 }
